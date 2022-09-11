e585ee85b4447e71b1f271b8b4f84f5c4309787a
Maia Cochran <maia.cochran1@gmail.com>
2022-09-11 13:54:51 -0700

    Setup

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
@@ -18,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+{/* <StatusBar style="auto" />  */}
