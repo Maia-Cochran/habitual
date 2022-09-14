716cd4311b3f696ab52b1e0ef80b3bbc83628943
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-09-14 18:57:04 -0500

    comment out unused files

--- components/Carousel/CustomSlider.js
+++ components/Carousel/CustomSlider.js
@@ -1,21 +1,21 @@
-import { Dimensions, View } from "react-native";
-import Carousel from "react-native-snap-carousel";
-import CarouselItem from "./CarouselItem";
-import styles from "./styles";
+// import { Dimensions, View } from "react-native";
+// import Carousel from "react-native-snap-carousel";
+// import CarouselItem from "./CarouselItem";
+// import styles from "./styles";
 
-const { width } = Dimensions.get("window");
-export default function CustomSlider({ goodVibe }) {
-  const settings = {
-    sliderWidth: width,
-    sliderHeight: width,
-    itemWidth: width - 80,
-    data: goodVibe,
-    renderItem: CarouselItem,
-    hasParallaxImages: true,
-  };
-  return (
-    <View style={styles.container}>
-      <Carousel {...settings} />
-    </View>
-  );
-}
+// const { width } = Dimensions.get("window");
+// export default function CustomSlider({ goodVibe }) {
+//   const settings = {
+//     sliderWidth: width,
+//     sliderHeight: width,
+//     itemWidth: width - 80,
+//     data: goodVibe,
+//     renderItem: CarouselItem,
+//     hasParallaxImages: true,
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <Carousel {...settings} />
+//     </View>
+//   );
+// }
