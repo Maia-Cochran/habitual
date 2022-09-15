5267372ddfb692b2eb5e2d4de7eadfaef7182dd5
Chantal Goethals <chantalgoethals@gmail.com>
2022-09-14 19:03:49 -0600

    Feat: move data to home screen

--- components/Carousel/GoodVibeView.js
+++ components/Carousel/GoodVibeView.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-// import { useState } from 'react';
+// import React from 'react';
+// import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+// // import { useState } from 'react';
 
-const GoodVibeView = () => {
-    let [quote, setQuote] = React.useState('')
-    let [source, setSource] = React.useState('')
-    const fetchApiCall = () => {
-     return fetch("http://localhost:3001/mantra")
-        .then(response => response.json())
-        .then(response => {
-          setQuote(response);
-          setSource(response)
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Native API Calls</Text>
-        <Text>Example with fetch and Axios</Text>
-        <TouchableHighlight onPress={fetchApiCall}>
-          <View style={styles.container}>
-            <Text style={styles.title}>{fetchApiCall}</Text>
-          </View>
-        </TouchableHighlight>
-        <View>
-          <Text>{quote}</Text>
-          <Text>{source}</Text>
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#AAA',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: '#fff'
-    },
-    title: {
-      fontSize: 35,
-      color: '#fff'
-    }
-  });
+// const GoodVibeView = () => {
+//     let [data, setData] = React.useState('')
+//     let [source, setSource] = React.useState('')
+//     const fetchApiCall = () => {
+//      return fetch("http://localhost:3001/mantra")
+//         .then(response => response.json())
+//         .then(response => {
+//           setQuote(response);
+//           setSource(response)
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
+//     }
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.title}>Native API Calls</Text>
+//         <Text>Example with fetch and Axios</Text>
+//         <TouchableHighlight onPress={fetchApiCall}>
+//           <View style={styles.container}>
+//             <Text style={styles.title}>{fetchApiCall}</Text>
+//           </View>
+//         </TouchableHighlight>
+//         <View>
+//           <Text>{quote}</Text>
+//           <Text>{source}</Text>
+//         </View>
+//         <StatusBar style="auto" />
+//       </View>
+//     );
+//   }
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#AAA',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//       color: '#fff'
+//     },
+//     title: {
+//       fontSize: 35,
+//       color: '#fff'
+//     }
+//   });
 
-export default GoodVibeView;
+// export default GoodVibeView;
--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,37 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableHighlight } from 'react-native'
 import Header from './Header'
 import GreetingButton from './GreetingButton'
 import Carousel from './Carousel/Carousel'
 import ModalTemplate from './ModalTemplate'
 import BottomNavBar from './BottomNavBar'
+import { Component } from 'react';
 
-const Home = () => {
+class Home extends Component  {
+    constructor() {
+        super();
+        this.state = {
+            text: ''
+    }
+}
+
+fetchApiCall = () => {
+    return fetch("http://localhost:3001/mantra")
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+
+render(props){
     return (
         <View style={styles.homeContainer}>
             <Header />
-            <GreetingButton />
+            <TouchableHighlight onPress={mantras}>  
+              <GreetingButton text={this.state.text}/>
+            </TouchableHighlight>
             <ModalTemplate />
             {/* ^^^ unsure of this location, does it exist in  */}
             {/* Home component or within carousel? need to determine */}
@@ -19,6 +41,7 @@ const Home = () => {
             </View>
         </View>
     )
+  }
 }
 
 export default Home;
