a245d0ff3cc0495c799ad6e08d4d2cde4005ed7d
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-09-15 13:00:58 -0500

    add modal to carousel card

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,8 +6,10 @@ import { SafeAreaView, Text, View } from 'react-native';
 import GoodVibeModal from '../Navigation/GoodVibeModal';
 import Carousel from 'react-native-snap-carousel'
 import GoodVibeView from '../CarouselCards/GoodVibeCard';
-import CheckListView from '../CarouselCards/CheckListCard';
+import CheckListCard from '../CarouselCards/CheckListCard';
 import CalendarView from '../CarouselCards/CalendarView';
+import GoodVibeCard from '../CarouselCards/GoodVibeCard';
+import ModalTemplate from '../ModalTemplate';
 
 
 class HabitualCarousel extends Component {
@@ -16,79 +18,86 @@ class HabitualCarousel extends Component {
     this.state = {
       activeIndex:0,
       carouselItems: [
-        // <GoodVibeView />,
-        // <CheckListView />,
-        // <CalendarView />
         {
-          id: 1,
-          title: "Tell Me Something Good",
-          icon:"🪷",
-          text: "Click here for a random compliment",
-      },
-      {
-        id: 2,
-        title: "Check your to do list",
-        icon:"📑",
-        text: "Click here to view your daily list",
-      },
-      {
-        id: 3,
-        title: "Calendar View",
-        icon:"📆",
-        text: "Click here to see your calendar",
-      },
-      {
-        id: 4,
-        title: "Favorite Vibes",
-        icon:"🥰",
-        text: "Click here to see your favorite vibes",
-      },
-
-        // <GoodVibeModal />,
-        <GoodVibeView />,
+          id: 1
+        },
+        // <GoodVibeCard 
+        //     // id: {1}
+        //     // title: {"Tell Me Something Good"}
+        //     // icon:"🪷",
+        //     // text: "Click here for a random compliment",
+        // />,
         // <CheckListView />,
         // <CalendarView />
       //   {
-          
-      //     title:"Item 1",
-      //     text: "Text 1",
+      //     id: 1,
+      //     // title: "Tell Me Something Good",
+      //     // icon:"🪷",
+      //     // text: "Click here for a random compliment",
       // },
       // {
-      //     title:"Item 2",
-      //     text: "Text 2",
+      //   id: 2,
+      //   // title: "Check your to do list",
+      //   // icon:"📑",
+      //   // text: "Click here to view your daily list",
       // },
       // {
-      //     title:"Item 3",
-      //     text: "Text 3",
+      //   id: 3,
+      //   // title: "Calendar View",
+      //   // icon:"📆",
+      //   // text: "Click here to see your calendar",
       // },
       // {
-      //     title:"Item 4",
-      //     text: "Text 4",
+      //   id: 4,
+      //   // title: "Favorite Vibes",
+      //   // icon:"🥰",
+      //   // text: "Click here to see your favorite vibes",
       // },
-      // {
-      //     title:"Item 5",
-      //     text: "Text 5",
-      // },
-
       ]
     }
   }
 
   _renderItem = ({ item, index }) => {
     return (
-      <View style={{
-        backgroundColor:'floralwhite',
-        borderRadius: 5,
-        height: 250,
-        padding: 50,
-        marginLeft: 25,
-        marginRight: 25, }}>
-
+      <View>
+        <View style={{
+          backgroundColor:'#3A376F',
+          color: 'white',
+          alignItems: 'center',
+          borderRadius: 16,
+          height: 400,
+          width: '100%',
+          padding: 40,
+          // marginLeft: 25,
+          // marginRight: 25, 
+        }}
+        >
+          <GoodVibeCard />
+        
           <Text style={{fontSize: 30}}>{item.title}</Text>
-        <Text style={{fontSize: 30}}>{item.icon}</Text>
-
-        <Text>{item.text}</Text>
-    </View>
+          <Text style={{fontSize: 30}}>{item.icon}</Text>
+          <Text>{item.text}</Text>
+        </View>
+        <View style={{
+          backgroundColor:'#3A376F',
+          color: 'white',
+          alignItems: 'center',
+          borderRadius: 16,
+          height: 400,
+          width: '100%',
+          padding: 40,
+          // marginLeft: 25,
+          // marginRight: 25, 
+        }}
+        >
+          <CheckListCard />
+        
+          <Text style={{fontSize: 30}}>{item.title}</Text>
+          <Text style={{fontSize: 30}}>{item.icon}</Text>
+          <Text>{item.text}</Text>
+        </View>
+      </View>
+    
     );
   }
 
@@ -96,12 +105,12 @@ class HabitualCarousel extends Component {
     return (
       <SafeAreaView style={{flex: 1,  }}>
 
-        <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+        <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', alignContents: 'center' }}>
         <Carousel
-            layout={"default"}
+            layout={"row"}
             ref={ref => this.carousel = ref}
             data={this.state.carouselItems}
-            sliderWidth={300}
+            sliderWidth={350}
             itemWidth={300}
             renderItem={this._renderItem}
             onSnapToItem = { index => this.setState({activeIndex:index}) } 
@@ -111,6 +120,14 @@ class HabitualCarousel extends Component {
     )
   }
 }
+
+// const styles = StyleSheet.create({
+//   textStyle: {
+//       fontSize: 30,
+//       color: 'FFFFFF',
+//       alignItems: 'center'
+//   }
+// })
 
 export default HabitualCarousel
 
--- components/CarouselCards/GoodVibeCard.js
+++ components/CarouselCards/GoodVibeCard.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import ModalTemplate from '../ModalTemplate';
 
-const GoodVibeView = () => {
+const GoodVibeCard = () => {
     return (
-        <View>
-            <Text style={styles.textStyle}>Tell Me Something Good</Text>
-            <Text style={styles.textStyle}>This is the carousel card that user can click to see the modal</Text>
-        </View>
-
+            <Pressable>
+                <View style={styles.card}>
+                    <Text style={styles.textStyle}>Tell Me Something Good</Text>
+                    <Text style={styles.textStyle}>This is the carousel card that user can click to see the modal</Text>
+                    <ModalTemplate />
+                </View>
+            </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30
-    }
+    },
+    // card: {
+    //     backgroundColor:'#3A376F',
+    //     color: 'white',
+    //     alignItems: 'center',
+    //     borderRadius: 16,
+    //     height: 400,
+    //     width: '88%',
+    //     padding: 40,
+    //     // marginLeft: 25,
+    //     // marginRight: 25, 
+    // }
 })
 
-export default GoodVibeView
+export default GoodVibeCard
