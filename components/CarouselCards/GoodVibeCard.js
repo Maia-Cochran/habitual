0ed79eba14840d14544437ffed8f7c1cd0330b79
Chantal Goethals <chantalgoethals@gmail.com>
2022-09-15 23:57:17 -0600

    Feat: add icons to btmnavbar

--- components/CarouselCards/GoodVibeCard.js
+++ components/CarouselCards/GoodVibeCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
 import { useState, useEffect } from 'react';
 import GoodVibeModal from '../Navigation/GoodVibeModal';
 import AppLoading from 'expo-app-loading';
@@ -38,9 +38,9 @@ const GoodVibeCard = () => {
         return <AppLoading />;
     } else{
     return (
-        <View style={styles.textContainer}>
+        <View style={styles.textContainer}>  
             <Text style={styles.textStyle}>Tell Me Something Good</Text>
-            <Text style={styles.textStyle}>🪷</Text>
+            <Image source={require("../../assets/zen.png")} style={styles.icons}/>
             <Text style={styles.textStyle}>Press Here for Your Dose of Good Vibes</Text>
             <View style={styles.goodVibeModalContainer}>
                 <View>
@@ -53,6 +53,12 @@ const GoodVibeCard = () => {
 };
 
 const styles = StyleSheet.create({
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 150,
+  },
     textStyle: {
         fontSize: 25,
         textAlign: 'center',
@@ -60,6 +66,10 @@ const styles = StyleSheet.create({
         padding: 10,
         fontFamily: 'IndieFlower_400Regular',
     },
+    icons:{
+      height: 38,
+  
+    }
 })
 
 export default GoodVibeCard
